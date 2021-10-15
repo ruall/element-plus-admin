644c730e6ed3e541731e54e6104fcd8a4a1e2501
ling <971039649@qq.com>
2021-10-15 17:14:09 +0800

    新增谷歌地图插件

--- src/views/Bud/BudApply.vue
+++ src/views/Bud/BudApply.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-        budApply
-    </div>
+  <div>
+    <GoogleMap
+      api-key="AIzaSyAAAdIjvwLUt30ZN8Q8pOkY1hAPzV3Klgw"
+      style="width: 100%; height: 500px"
+      :center="center"
+      :libraries="['geometry', 'drawing', 'places']"
+      @dragend="getPoint"
+      :zoom="15"
+    >
+      <Marker :options="markerOptions" />
+    </GoogleMap>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { GoogleMap, Marker } from 'vue3-google-map'
 export default defineComponent({
-    name: 'BudApply'
+  components: { GoogleMap, Marker },
+  name: 'BudApply',
+  setup() {
+    const center = { lat: 40.689247, lng: -74.044502 }
+    const markerOptions = { position: center, label: 'L', title: 'LADY LIBERTY', draggable: true }
+    const getPoint = () => {
+      console.log('111')
+    }
+    return { center, markerOptions, getPoint }
+  }
 })
 </script>
-
